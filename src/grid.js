b3914a8bceb081e2c15be873d76967fc38d9a80d
CANHermit <37168672+CANHermit@users.noreply.github.com>
2019-05-05 17:48:44 -0700

    Update grid.js
    Updated amount of circles in grid

--- src/grid.js
+++ src/grid.js
@@ -17,16 +17,61 @@ class Grid extends React.Component {
           {this.renderCircle(0)}
           {this.renderCircle(1)}
           {this.renderCircle(2)}
-        </div>
-        <div className="grid-row">
           {this.renderCircle(3)}
           {this.renderCircle(4)}
           {this.renderCircle(5)}
+          {this.renderCircle(6)}
+       
         </div>
         <div className="grid-row">
-          {this.renderCircle(6)}
           {this.renderCircle(7)}
           {this.renderCircle(8)}
+          {this.renderCircle(9)}
+          {this.renderCircle(10)}
+          {this.renderCircle(11)}
+          {this.renderCircle(12)}
+          {this.renderCircle(13)}
+    
+        </div>
+        <div className="grid-row">
+          {this.renderCircle(14)}
+          {this.renderCircle(15)}
+          {this.renderCircle(16)}
+          {this.renderCircle(17)}
+          {this.renderCircle(18)}
+          {this.renderCircle(19)}
+          {this.renderCircle(20)}
+   
+        </div>
+        <div className="grid-row">
+          {this.renderCircle(21)}
+          {this.renderCircle(22)}
+          {this.renderCircle(23)}
+          {this.renderCircle(24)}
+          {this.renderCircle(25)}
+          {this.renderCircle(26)}
+          {this.renderCircle(27)}
+  
+        </div>
+        <div className="grid-row">
+          {this.renderCircle(28)}
+          {this.renderCircle(29)}
+          {this.renderCircle(30)}
+          {this.renderCircle(31)}
+          {this.renderCircle(32)}
+          {this.renderCircle(33)}
+          {this.renderCircle(34)}
+     
+        </div>
+          <div className="grid-row">
+          {this.renderCircle(35)}
+          {this.renderCircle(36)}
+          {this.renderCircle(37)}
+          {this.renderCircle(38)}
+          {this.renderCircle(39)}
+          {this.renderCircle(40)}
+          {this.renderCircle(41)}
+         
         </div>
       </div>
     );
