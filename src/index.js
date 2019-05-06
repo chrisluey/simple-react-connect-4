761fffc9c07b670a70c76ce2d5da883aa71e5786
CANHermit <37168672+CANHermit@users.noreply.github.com>
2019-05-05 20:40:11 -0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import Grid from './grid.js';
+import Board from './board.js';
 
 class Game extends React.Component {
   render() {
     return (
       <div className="game">
-        <div className="game-grid">
-          <Grid />
+        <div className="game-board">
+          <Board />
         </div>
         <div className="game-info">
           <div>{/* status */}</div>
