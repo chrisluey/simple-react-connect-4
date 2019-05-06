aeb2eab7fb16bcb29e0512ce2423bc828fa8c87b
chrisluey <41104962+chrisluey@users.noreply.github.com>
2019-05-05 17:34:43 -0700

    File refactor
    Refactored the React components to be in different files for readability and for potential merge conflicts in the future

---
+++ src/circle.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './index.css';
+
+class Circle extends React.Component {
+  render() {
+    return (
+      <button className="circle">
+        {/* TODO */}
+      </button>
+    );
+  }
+}
+
+export default Circle;
