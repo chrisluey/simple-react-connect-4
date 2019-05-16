aeb97ca4e15f664bcf61684a5e76edd19fd8f50a
CANHermit <37168672+CANHermit@users.noreply.github.com>
2019-05-15 22:38:09 -0700

    Add files via upload
    reset button test

--- src/board.js
+++ src/board.js
@@ -1,29 +1,20 @@
-import React from 'react';
-import './index.css';
-import Square from './square.js';
+import React from "react";
+import "./index.css";
+import Square from "./square.js";
+import { restElement } from "@babel/types";
 
 class Board extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-        squares: Array(42).fill(null),
-        p1IsNext: true,
+      squares: Array(42).fill(null),
+      p1IsNext: true
     };
   }
 
-  calculateWinner() {
-    let x = 0;
-  }
-
   renderSquare(i) {
-      return (
-      <Square
-        number={i}
-        value={this.state.squares[i]}
-      />
-    );
+    return <Square number={i} value={this.state.squares[i]} />;
   }
-
   findLowestSquare(i) {
     const squares = this.state.squares.slice();
     let x = i * 6;
@@ -40,16 +31,14 @@ class Board extends React.Component {
   handleClick(i) {
     const squares = this.state.squares.slice();
     let n = this.findLowestSquare(i);
-    if (n !== -1) {
-      squares[n] = this.state.p1IsNext ? 'P1' : 'P2';
-    }
-    else
-    {
-      alert('Column is full');
+    if (n != -1) {
+      squares[n] = this.state.p1IsNext ? "P1" : "P2";
+    } else {
+      alert("Column is full");
     }
     this.setState({
       squares: squares,
-      p1IsNext: !this.state.p1IsNext,
+      p1IsNext: !this.state.p1IsNext
     });
   }
 
@@ -57,36 +46,46 @@ class Board extends React.Component {
     let x = i * 6;
     return (
       <div className="board-column" onClick={() => this.handleClick(i)}>
-            {this.renderSquare(x)}
-            {this.renderSquare(x + 1)}
-            {this.renderSquare(x + 2)}
-            {this.renderSquare(x + 3)}
-            {this.renderSquare(x + 4)}
-            {this.renderSquare(x + 5)}
+        {this.renderSquare(x)}
+        {this.renderSquare(x + 1)}
+        {this.renderSquare(x + 2)}
+        {this.renderSquare(x + 3)}
+        {this.renderSquare(x + 4)}
+        {this.renderSquare(x + 5)}
       </div>
-    )
+    );
   }
 
   render() {
-    let status = 'Next player: ' + (this.state.p1IsNext ? 'P1' : 'P2');
+    let status =
+      "Next player: " + (this.state.p1IsNext ? "HachimanHikigaya" : "Langara");
 
     return (
       <div>
         <div className="status">{status}</div>
         <div className="board">
-
-        {this.renderColumn(0)}
-        {this.renderColumn(1)}
-        {this.renderColumn(2)}
-        {this.renderColumn(3)}
-        {this.renderColumn(4)}
-        {this.renderColumn(5)}
-        {this.renderColumn(6)}
-
+          {this.renderColumn(0)}
+          {this.renderColumn(1)}
+          {this.renderColumn(2)}
+          {this.renderColumn(3)}
+          {this.renderColumn(4)}
+          {this.renderColumn(5)}
+          {this.renderColumn(6)}
         </div>
+        <button className="resetbutton" onClick={() => this.resetButton()}>
+          {" "}
+          Reset{" "}
+        </button>
       </div>
     );
   }
+  resetButton = () => {
+    this.state = {
+      squares: Array(42).fill(null),
+      p1IsNext: true
+    };
+    this.render();
+  };
 }
 
 export default Board;
