1febf9922e0c05ea3ba4a19c600684082e2efe8b
chrisluey <41104962+chrisluey@users.noreply.github.com>
2019-05-15 21:10:54 -0700

    Added on-click function

--- src/board.js
+++ src/board.js
@@ -7,31 +7,41 @@ class Board extends React.Component {
     super(props);
     this.state = {
         squares: Array(42).fill(null),
-        xIsNext: true
+        p1IsNext: true,
     };
   }
 
   renderSquare(i) {
-    return <Square number={i}/>;
+      return (
+      <Square
+        number={i}
+        value={this.state.squares[i]}
+      />
+    );
+  }
+
+  handleClick(i) {
+    this.setState({
+      p1IsNext: !this.state.p1IsNext,
+    });
   }
 
   renderColumn(i) {
-    var x = i *6;
+    let x = i * 6;
     return (
-      <div className="board-column" onClick={function() { alert('column was clicked');}}>
+      <div className="board-column" onClick={() => this.handleClick(i)}>
             {this.renderSquare(x)}
             {this.renderSquare(x + 1)}
             {this.renderSquare(x + 2)}
             {this.renderSquare(x + 3)}
             {this.renderSquare(x + 4)}
             {this.renderSquare(x + 5)}
-  
-          </div>
+      </div>
     )
   }
 
   render() {
-    const status = 'Next player: X';
+    let status = 'Next player: ' + (this.state.p1IsNext ? 'P1' : 'P2');
 
     return (
       <div>
